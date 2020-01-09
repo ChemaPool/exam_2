a529f61b36f636eb4c695cef4f59190bba300225
ChemaPool <jose_development@hotmail.com>
2020-01-09 10:11:45 -0600

    Home

--- app/models/similar_home.rb
+++ app/models/similar_home.rb
@@ -9,7 +9,7 @@ class SimilarHome
 
     owners_id.each do |id|
       owner = Owner.find(id)
-      owner_home = Home.where(owner_id: id, master_home_id: nil).pluck(:id)
+      owner_home = Home.where(owner_id: id).pluck(:id)
 
       next if owner_home.length == 1
 
@@ -17,20 +17,14 @@ class SimilarHome
         if index + 1 < owner_home.length
 
           firts_home = Home.find(id: home)
+          next if firts_home.master_home_id != nil
           next unless firts_home.published?
 
           next_home = owner_home[index + 1]
           second_home = Home.find(id: next_home)
-          next unless firts_home.total_amount == second_home.total_amount
-          next unless firts_home.location == second_home.location
-
-          puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-          puts "Nombre Propietario: #{owner.user.name}"
-          puts "Correo Propietario: #{owner.user.email}"
-          puts "ID Depa: #{firts_home.id}"
-          puts "Precio Depa: #{firts_home.total_amount}"
-          puts "Estatus Depa: #{firts_home.status}"
-          puts "Location Depa: #{firts_home.location}"
+          if firts_home.total_amount == second_home.total_amount && firts_home.location == second_home.location
+            puts "#{owner.user.name}, #{owner.user.email}, #{firts_home.id}, #{firts_home.total_amount}, #{firts_home.status}, #{firts_home.location}"
+          end
         end
       end
     end
